d9a318a75e5b73c589e01346f87c5d7e62992784
krishanthisera <krishees@gmail.com>
2020-12-27 19:34:23 +1030

    shared libraries

--- subrotine.groovy
+++
@@ -1,7 +0,0 @@
-String getUsername(String firstname, String lastname){
-    return firstname.toLowerCase() + lastname.toLowerCase();
-}
-
-assert getUsername("krishan", "Thisera") == "krishanthisera" : "Invalid"
-
-println(getUsername("krishan", "Thisera"))
