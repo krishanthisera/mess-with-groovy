922e80d33fcdb05cdc4f29a3273d54e60d6ae2c5
krishanthisera <krishees@gmail.com>
2020-12-28 14:02:43 +1030

    date function fix

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,8 +1,9 @@
 import java.io.*;
 import groovy.io.*;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-@NonCPS
+// @NonCPS
 def call(Map config=[:]){
     def dir = new File(pwd());
     new File(dir.path + "/releasenote.md").withWriter('utf-8')
@@ -17,7 +18,24 @@ def call(Map config=[:]){
                 writer.writeLine("\t" + "(f)" + file.name + "--->" + file.length());
         }
     }
+    // Jenkins console out the date
     def date = new Date();
     def sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
     echo "The date is: " + sdf.format(date)
+    // Jenkins console out the build number
+    echo "The build number is: " + '${BUILD_NUMBER}'
+    //SCM Change logs
+    def changeLogSets = currentBuild.changeSets;
+
+    if(config.change != "false"){
+        for ( change in changeLogSets){
+            def entries = change.items;
+            for (entry in entries){
+                echo "${entry.commitID} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+                for (file in entry.affectedFiles){
+                    echo "\t ${file.editType.name} ${file.path}";
+                }
+            }
+        }
+    }
 }
